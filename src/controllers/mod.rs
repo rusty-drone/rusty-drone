4def75f6ce3263ec76b272a487931a7213adde95
KaustubhKhulbe <kaustubh.khulbe@gmail.com>
2022-03-05 19:54:32 -0800

    proportional controller

--- src/controllers/mod.rs
+++ src/controllers/mod.rs
@@ -5,3 +5,9 @@ pub fn error<O: AddAssign + Add + Sub + Mul + Div + Copy, S: Stream<T = O>, P: S
 where <O as Sub>::Output: AddAssign + Copy + Sub + Mul + Div + Add{
     s.zip(p, |x: S::T, y: P::T| {y - x})
 }
+
+
+pub fn proportional_controller<O: AddAssign + Add + Sub + Mul + Div + Copy, S: Stream<T = O>> (s: S, p: O) -> impl Stream<T = <O as Mul>::Output>
+where <O as Mul>::Output: AddAssign + Copy + Sub + Mul + Div + Add{
+    s.map(move |x: S::T| {x * p})
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use rust_architecture::streams::{self, Stream};
-
+use rust_architecture::controllers::{self, error, proportional_controller};
 fn main() {
     println!("Hello, world!");
     let mut x = streams::ConstantStream::new(1).map(|x| x + 1);
-    std::println!("{:?}", x.next());
-    std::println!("{:?}", x.next());
-    std::println!("{:?}", x.next());
-    std::println!("{:?}", x.next());
+    let mut y = streams::ConstantStream::new(8);
+    let mut z = error(x, y);
+
+    let mut prop = proportional_controller(z, 2);
+    std::println!("{:?}", prop.next());
 }
