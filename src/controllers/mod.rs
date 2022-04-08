beff6733fa06573951464f6586a1965223430690
KaustubhKhulbe <kaustubh.khulbe@gmail.com>
2022-04-08 13:26:48 -0600

    cleanup

--- src/controllers/mod.rs
+++ src/controllers/mod.rs
@@ -38,38 +38,3 @@ where  O: Sub<Output = O> + Div<Output = O> + Copy, T: Sub<Output = O>, <O as Mu
     );
     out
 }   
-
-//TODO(#1): update this to use TimeStamped
-
-// pub fn derivative_controller<O: AddAssign + Add + Sub + Mul + Div + Copy, S: Stream<T = O>> ( mut s: SlideStream<S, O>, d: O) -> 
-// impl Stream<T = <<O as Sub>::Output as Mul>::Output> 
-// where <<O as Sub>::Output as Mul>::Output: AddAssign + Copy + Sub + Mul + Div + Add,
-//  <O as Sub>::Output: Mul,
-//  O: Sub<Output = O>{
-//     if s.get_size() <= 1 {
-//         panic!("SlideStream window is not of compatible size for derivative operation!");
-//     }
-
-//     return CustomStream::new( move || {
-//         // s.next() is placed at size - 1, so the previous value is at size - 2. This is why the window must be greater than 1
-//         (s.next() - s.get_data()[s.get_size() - 2]) * d
-//     });
-// }
-//`TimeStamped<T, O>`: `<Output = TimeStamped<T, O>>`
-// pub fn derivative_controller<O: AddAssign + Add + Sub + Mul + Div + Copy, T: AddAssign + Add + Sub + Mul + Div + Copy, S: Stream<T = T>, P: Stream<T = O>> ( data: S, time: P, d: T) -> impl Stream<T = TimeStamped<<T as Mul>::Output, O>> 
-// where <<T as Mul>::Output as Div>::Output : AddAssign + Add + Sub + Mul + Div + Copy, <T as Sub>::Output: AddAssign + Add + Sub + Mul + Div + Copy, T: Sub<Output = T>, O: Sub<Output = T>, <T as Mul>::Output: AddAssign + Add + Sub + Mul + Div + Copy {
-//     let mut zip = zip_with_time(time, data);
-//     let slide = SlideStream::new(zip.clone(), 2, zip.next());
-//     let clone = slide.clone();
-//     let out = slide.map(move |next| {
-        
-//         let change_in_data = next.value - clone.get_data()[clone.get_size() - 2].value;
-//         let change_in_time = next.time - clone.get_data()[clone.get_size() - 2].time;
-        
-//         let o = d * change_in_data / change_in_time;
-
-//         TimeStamped{value: o * d, time: next.time}
-//     }
-//     );
-//     return out;
-// }
--- src/main.rs
+++ src/main.rs
@@ -7,22 +7,6 @@ fn main() {
 
     let time = std::time::Instant::now();
 
-    // let s = ConstantStream::new(5);
-    // let time = CustomStream::new(|| {
-    //     let t = match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
-    //         Ok(t) => t,
-    //         Err(_) => panic!("SystemTime before UNIX EPOCH!"),
-    //     };
-    //     std::thread::sleep(time::Duration::from_millis(100));
-    //     t.as_millis()
-    // });
-    // let mut d = derivative_controller(s, time, 1);
-    // let v = d.next();
-    // println!("{:?}", d.next());
-    // println!("{:?}", d.next());
-    // println!("{:?}", d.next());
-    // println!("{:?}", d.next());
-
     loop {
         handler.run();
         if std::time::Instant::now().duration_since(time) > time::Duration::from_millis(5000) {
