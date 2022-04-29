785796b6495143b2e2fa55a095b3525c5f2621c9
KaustubhKhulbe <kaustubh.khulbe@gmail.com>
2022-04-29 12:25:58 -0700

    it actually does work

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use rust_architecture::{events::{event_handler::EventHandler, impulse_event::ImpulseEvent, continuous_event::ContinuousEvent}, tasks::finite_task::FiniteTask};
+use rust_architecture::{events::{event_handler::EventHandler, impulse_event::ImpulseEvent, continuous_event::ContinuousEvent}, tasks::finite_task::FiniteTask, streams::{constant_stream::ConstantStream, stream::Stream}, components::{pure_component::PureComponent, component::Component}};
 use core::time;
 use std::{time::Instant, cell::RefCell, rc::Rc};
 extern crate num_traits;
@@ -20,8 +20,20 @@ fn main() {
 
     handler.add_event(Box::new(e));
 
+    let s1 = ConstantStream::<f64>::new(5.0);
+    let s2 = s1.map(|x| {2.0 * x});
+    let s3 = s1.map(|x| {3.0 * x});
+
+    let mut comp = PureComponent::new(Box::new(s2), Box::new(s3), |x| {println!("Yo + {}", x)});
+    comp.reset_to_default();
+
+    let s4 = s1.map(|x| {x*x});
+
+    comp.set_controller(Box::new(s4));
+
     loop {
         handler.update_events();
+        comp.apply_signal();
 
         if Instant::now().duration_since(initial).as_millis() > 5000 {
             handler.shut_down();
