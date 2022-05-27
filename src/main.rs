f873efda0e0e7fd0e7a7e600b182d8ecd390912c
KaustubhKhulbe <kaustubh.khulbe@gmail.com>
2022-05-26 21:07:45 -0700

    wip replacing task workflow

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,7 @@
-use rust_architecture::{events::{event_handler::EventHandler, impulse_event::ImpulseEvent}, streams::{constant_stream::ConstantStream, stream::Stream}, components::{pure_component::PureComponent, component::Component}, tasks::{finite_task::FiniteTask, task::Task, sequential_task::SequentialTask}};
+use rust_architecture::{events::{event_handler::EventHandler, impulse_event::ImpulseEvent}, streams::{constant_stream::ConstantStream, stream::Stream}, components::{pure_component::PureComponent, component::Component}, tasks::{finite_task::FiniteTask, task::Task, sequential_task::SequentialTask, parallel_task::ParallelTask}};
 use core::time;
 use std::{time::{Instant, Duration}, cell::RefCell, rc::Rc, thread::{sleep_ms, sleep}};
 extern crate num_traits;
-
-// fn main() {
-
-//     let mut handler = EventHandler::new(time::Duration::from_millis(200));
-
-//     let initial = Instant::now();
-
-//     let s1 = ConstantStream::<f64>::new(12.0);
-//     let s2 = s1.map(|x| {2.0 * x});
-//     let s3 = s1.map(|x| {3.0 * x});
-
-//     let comp= Rc::new(RefCell::new(PureComponent::new(Box::new(s2), Box::new(s3), |x| {println!("{}", x)})));
-
-//     let c = comp.clone();
-//     let c2 = comp.clone();
-
-//     let actions_2 = vec![Box::new( 
-//         move || {(*c.borrow_mut()).reset_to_default();},
-//     )as Box<dyn FnMut()>, Box::new( || {println!("2nd action");} )as Box<dyn FnMut()>];
-
-//     let actions_3 = vec![Box::new( 
-//         move || {(*c2.borrow_mut()).set_controller(Box::new(s1));},
-//     )as Box<dyn FnMut()>];
-
-//     let imp2 = ImpulseEvent::new(move | | Instant::now().duration_since(initial).as_millis() > 3000, actions_2);
-//     let imp3 = ImpulseEvent::new(move | | Instant::now().duration_since(initial).as_millis() > 2000, actions_3);
-
-//     handler.add_event(Box::new(imp2));
-//     handler.add_event(Box::new(imp3));
-
-//     loop {
-//         handler.update_events();
-//         (*comp.borrow_mut()).apply_signal();
-
-//         if Instant::now().duration_since(initial).as_millis() > 5000 {
-//             handler.shut_down();
-//             break;
-//         }
-//     }
-// }
 
 fn main() {
 
--- src/tasks/parallel_task.rs
+++ src/tasks/parallel_task.rs
@@ -1,51 +1,49 @@
-// use crate::tasks::task::Task;
+use crate::tasks::task::Task;
 
-// /**
-//  * Two tasks that are run simultaneously.
-//  */
-// pub struct ParallelTask {
-//     pub first: Box<dyn Task>,
-//     pub second: Box<dyn Task>,
-//     first_finished: bool,
-//     second_finished: bool,
-// }
+/**
+ * Two tasks that are run simultaneously.
+ */
+pub struct ParallelTask {
+    pub first: Box<dyn Task>,
+    pub second: Box<dyn Task>,
+    first_finished: bool,
+    second_finished: bool,
+    first_init: bool,
+    second_init: bool,
+}
 
-// impl ParallelTask {
-//     pub fn new(first: Box<dyn Task>, second: Box<dyn Task>) -> ParallelTask{
-//         ParallelTask { first, second, first_finished: false, second_finished: false }
-//     }
-// }
+impl ParallelTask {
+    pub fn new(first: Box<dyn Task>, second: Box<dyn Task>) -> ParallelTask{
+        ParallelTask { first, second, first_finished: false, second_finished: false, first_init: false, second_init: false }
+    }
+}
 
-// impl Task for ParallelTask {
-//     fn initialize(&mut self) {
-//         self.first.initialize();
-//         self.second.initialize();
-//     }
+impl Task for ParallelTask {
+    fn initialize(&mut self) {
+        if !self.first_init {
+            self.first.initialize();
+            self.first_init = true;
+        }
+        if !self.second_init {
+            self.second.initialize();
+            self.second_init = true;
+        }
 
-//     fn execute(&mut self) {
-//         if !self.first_finished {
-//             self.first.execute();
-//         }
+        if self.first.has_finished() && !self.first_finished {
+            self.first_finished = true;
+            self.first.end();
+        }
 
-//         if !self.second_finished {
-//             self.second.execute();
-//         }
+        if self.second.has_finished() && !self.second_finished {
+            self.second_finished = true;
+            self.second.end();
+        }
+    }
 
-//         if self.first.has_finished() && !self.first_finished {
-//             self.first_finished = true;
-//             self.first.end();
-//         }
+    fn end(&mut self) {
+    }
 
-//         if self.second.has_finished() && !self.second_finished {
-//             self.second_finished = true;
-//             self.second.end();
-//         }
-//     }
-
-//     fn end(&mut self) {
-//     }
-
-//     fn has_finished(&mut self) -> bool {
-//         self.first_finished && self.second_finished
-//     }
-// }
+    fn has_finished(&mut self) -> bool {
+        self.first_finished && self.second_finished
+    }
+}
